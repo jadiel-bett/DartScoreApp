e7123db42f7ebb0f62b909faf392a1aa42a2c443
jadiel-bett <jadielbett@gmail.com>
2024-11-27 10:58:24 +0300

    Refactor code

--- dart_score_app/test/widget_test.dart
+++ dart_score_app/test/widget_test.dart
@@ -5,15 +5,16 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:dart_score_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dart_score_app/main.dart';
+// import 'package:dart_score_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HomeScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
